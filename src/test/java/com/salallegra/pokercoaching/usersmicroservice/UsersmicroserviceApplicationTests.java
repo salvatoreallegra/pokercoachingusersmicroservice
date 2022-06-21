b6919cc70915cc23ebae115fc8926b16b67d5a6f
salallegra <salvatore.allegra@smoothstack.com>
2022-06-20 18:47:43 -0700

    Initial commit of users microservice
    Initial Commit

---
+++ src/test/java/com/salallegra/pokercoaching/usersmicroservice/UsersmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salallegra.pokercoaching.usersmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
