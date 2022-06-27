5a8c3dfbf32aa68f479ff240124cdab683bd0a6f
salallegra <salvatore.allegra@smoothstack.com>
2022-06-27 16:49:41 -0700

    Add load balancing for multiple instances

--- src/main/java/com/pokercoaching/usersmicroservice/UsersmicroserviceApplication.java
+++ src/main/java/com/pokercoaching/usersmicroservice/UsersmicroserviceApplication.java
@@ -3,6 +3,7 @@ package com.pokercoaching.usersmicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableDiscoveryClient
