db04ef41522d521f179ada02c51741a9efadf2a2
salallegra <salvatore.allegra@smoothstack.com>
2022-06-29 17:50:26 -0700

    Create UserRepository.java

---
+++ src/main/java/com/pokercoaching/usersmicroservice/data/UserRepository.java
@@ -0,0 +1,8 @@
+package com.pokercoaching.usersmicroservice.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    
+}
