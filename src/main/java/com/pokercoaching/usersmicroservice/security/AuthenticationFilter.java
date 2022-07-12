1f5d807121ff8e7a3a6e9ccf6ce4bf14ad6ec0c1
salallegra <salvatore.allegra@smoothstack.com>
2022-07-11 22:35:02 -0700

    Add login and return jwt token

--- src/main/java/com/pokercoaching/usersmicroservice/security/AuthenticationFilter.java
+++ src/main/java/com/pokercoaching/usersmicroservice/security/AuthenticationFilter.java
@@ -2,9 +2,17 @@ package com.pokercoaching.usersmicroservice.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pokercoaching.usersmicroservice.models.LoginRequestModel;
+import com.pokercoaching.usersmicroservice.services.UsersService;
+import com.pokercoaching.usersmicroservice.shared.UserDto;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -13,8 +21,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Autowired
+    private UsersService usersService;
+
+    @Autowired
+    private Environment environment;
+
+    public AuthenticationFilter(UsersService usersService, Environment environment, AuthenticationManager authenticationManager){
+        this.usersService = usersService;
+        this.environment = environment;
+        super.setAuthenticationManager(authenticationManager);
+    }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest req,
@@ -41,17 +62,20 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                                             HttpServletResponse res,
                                             FilterChain chain,
                                             Authentication auth) throws IOException, ServletException {
-       /* String userName = ((User) auth.getPrincipal()).getUsername();
+
+        //return JWT token to client so it can use the token on subsequent requests
+
+        String userName = ((User) auth.getPrincipal()).getUsername();
         UserDto userDetails = usersService.getUserDetailsByEmail(userName);
 
         String token = Jwts.builder()
                 .setSubject(userDetails.getUserId())
                 .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(environment.getProperty("token.expiration_time"))))
-                .signWith(SignatureAlgorithm.HS512, environment.getProperty("token.secret") )
+                .signWith( SignatureAlgorithm.HS512, environment.getProperty("token.secret") )
                 .compact();
 
         res.addHeader("token", token);
-        res.addHeader("userId", userDetails.getUserId());*/
+        res.addHeader("userId", userDetails.getUserId());
     }
 
 }
--- src/main/java/com/pokercoaching/usersmicroservice/services/UsersServiceImpl.java
+++ src/main/java/com/pokercoaching/usersmicroservice/services/UsersServiceImpl.java
@@ -38,6 +38,16 @@ public class UsersServiceImpl implements UsersService {
         UserDto returnValue = mapper.map(userEntity,UserDto.class);
         return returnValue;
     }
+
+    @Override
+    public UserDto getUserDetailsByEmail(String email) {
+
+        UserEntity userEntity = userRepository.findByEmail(email);
+        if(userEntity == null) throw new UsernameNotFoundException(email);
+
+        return new ModelMapper().map(userEntity,UserDto.class);
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
@@ -45,4 +55,5 @@ public class UsersServiceImpl implements UsersService {
         if(userEntity == null) throw new UsernameNotFoundException(username);
         return new User(userEntity.getEmail(),userEntity.getEncryptedPassword(), true,true,true,true, new ArrayList<>());
     }
+
 }
